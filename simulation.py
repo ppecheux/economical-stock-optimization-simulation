e88b6bc3a9c2dfed4795eecd33032dc373393f15
ppecheux <ppecheux@etu.utc.fr>
2018-11-30 20:24:08 +0100

    donne un graph mais en changeant des param ne fonctionne pas

--- simulation.py
+++ simulation.py
@@ -6,16 +6,12 @@ On s’intéresse au stock de produits P1, le plus vendu.
 On considère que la demande hebdomadaire de produits 
 P1 peut être modélisée par une loi Normale N(155,38.9)
 
-'''
+stock de sécurité : SS= alpha*sigma
+demande ~ N(155,60)
+(demande-155)/(60/sqrt(n)) ~ N(0,1)
+P(-U(1-a/2)<demande-155)/(60/sqrt(n))< U(1-a/2))=1-a
 
-'''question 11
-
-il faut liquider apres 4 semaines
-
-on a un niveau de service cible SC
-
-on doit déterminer la proportion de produit à se débarasser chaque semaine
-'''
+alpha = étant fixé
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -25,12 +21,12 @@ class Stock:
 
 
     def __init__(self,serviceCible,stockCible,ListeSemainesDeStock):
-        self.sc=serviceCible
-        self.semaines=ListeSemainesDeStock
-        self.dechet=0#on aurait pu mettre comme une semaine suplémentaire...
-        self.nbNonSatifait = 0
-        self.stockCible = stockCible
-        self.fournis= self.stockTotal()
+        self.sc=float(serviceCible)#proportion de clients servis
+        self.semaines=ListeSemainesDeStock#liste de stock
+        self.dechet=0.0#on aurait pu mettre comme une semaine suplémentaire...
+        self.nbNonSatifait = 0.0
+        self.stockCible = float(stockCible)
+        self.fournis= float(self.stockTotal())
 
     def stockTotal(self):
         somme=0
@@ -40,7 +36,7 @@ class Stock:
 
     def nouvelleSemaine(self,demande):
         #On cherche à répondre à la demande
-        #On considère que chaque client achete un produit
+        #On considère qu'on doit livrer qu'un seul client
         temp=[]
         for i in self.semaines:
             
@@ -50,9 +46,6 @@ class Stock:
             else:
                 demande-=i
                 i=0
-            
-            #pour voir le stock de la semaine i:
-            #print(i)
 
             temp.append(i)
 
@@ -74,65 +67,81 @@ class Stock:
         print("\nmon stock total actuel est de ")
         print(self.stockTotal())
 
-mu, sigma = 155, 60 # demande moyenne et equart type de la demande moyenne
+
 nbSemPermenption = 3
 ListeSemainesDeStock = np.zeros(nbSemPermenption)
 
 
 
-def simulerSemainesStockCible(nbSemaines,stockCible):
+
+
+def simulerSemainesStockCible(nbSemaines,stockCible,demandes):
     ListeSemainesDeStock[0]=stockCible
     monStock=Stock(1,stockCible,ListeSemainesDeStock)
-    demandes = np.random.normal(mu, sigma, nbSemaines)
 
     for i in demandes:
         monStock.nouvelleSemaine(i)
 
-    print(nbsemaine)
-    print(monStock.nbNonSatifait)
-    serviceCibleReel = 1 - (float(monStock.nbNonSatifait)/nbSemaines)
-    print(serviceCibleReel)
+    serviceCibleReel = float(1 - (float(monStock.nbNonSatifait)/nbSemaines))
+   
     return serviceCibleReel
 
-def dechetsSemainesStockCible(nbSemaines,stockCible):
+def dechetsSemainesStockCible(nbSemaines,stockCible,demandes):
     ListeSemainesDeStock[0]=stockCible
     monStock=Stock(1,stockCible,ListeSemainesDeStock)
-    demandes = np.random.normal(mu, sigma, nbSemaines)
+
     for i in demandes:
         monStock.nouvelleSemaine(i)
     
-    dechet = monStock.dechet/monStock.fournis
-    return dechet#semble trop faible
+    tauxDechet=0.0
+    try:
+        tauxDechet = float(monStock.dechet)/monStock.fournis
+    except:
+        return -1
 
-def simulerTabStockCible(nbSemaines,tabStockCible):
+    print([tauxDechet,monStock.dechet,monStock.fournis,monStock.stockTotal()])
+    return tauxDechet#semble trop faible
+
+#dechetsSemainesStockCible(10,155)
+
+def simulerTabStockCible(nbSemaines,tabStockCible,mesDemandes):
     tabService = []
     for i in tabStockCible:
-        tabService.append( simulerSemainesStockCible(nbSemaines,i))
+        tabService.append( simulerSemainesStockCible(nbSemaines,i,mesDemandes))
     return(tabService)
 
-def dechetTabStockCible(nbSemaines,tabStockCible):
+def dechetTabStockCible(nbSemaines,tabStockCible,mesDemandes):
     tabDechet = []
     for i in tabStockCible:
-        tabDechet.append( dechetsSemainesStockCible(nbSemaines,i))
+        tabDechet.append( dechetsSemainesStockCible(nbSemaines,i,mesDemandes))
     return(tabDechet)
 
-tabStockCible=np.arange(0,500,10)
-nbsemaine=100
-tabServiceSimule =simulerTabStockCible(nbsemaine,tabStockCible)
-dechets = dechetTabStockCible(nbsemaine,tabStockCible)
+#dechetTabStockCible(10,[3,2],[1550,0])
 
-plt.ylabel("Service Reel Simule")
-plt.xlabel("Stock Cible")
-plt.plot(tabStockCible,tabServiceSimule,'ro')
+def simulationPousse():
+
+    tabStockCible=np.arange(0,500,10)
+    mu, sigma = 155, 60 # demande moyenne et equart type de la demande moyenne
+
+    nbsemaine=10000
+    mesDemandes = np.random.normal(mu, sigma, nbsemaine)
+
+    tabServiceSimule =simulerTabStockCible(nbsemaine,tabStockCible,mesDemandes)
+    dechets = dechetTabStockCible(nbsemaine,tabStockCible,mesDemandes)
+
+    plt.ylabel("Service Reel Simule")
+    plt.xlabel("Stock Cible")
+    plt.plot(tabStockCible,tabServiceSimule,'ro')
+    plt.plot(tabStockCible,dechets)
+    print(tabServiceSimule)
+    print(dechets)
+    plt.show()
+
+simulationPousse()
 
 
-plt.plot(tabStockCible,dechets)
-print(tabServiceSimule)
-print(dechets)
 
 
-
-plt.show()
 #print(dechetsSemainesStockCible(100,200))
 
 
